7dcfa6c702251f6d203177b59740bec76d472704
FranzPoize <francois.poizat@gmail.com>
2013-03-23 16:24:08 +0100

    added world singleton

---
+++ MaybeAwkward/MaybeAwkward/World.cpp
@@ -0,0 +1,5 @@
+#include "World.h"
+
+using namespace MA;
+
+World World::instance;
--- MaybeAwkward/MaybeAwkward/World.h
+++ MaybeAwkward/MaybeAwkward/World.h
@@ -1,12 +1,22 @@
 #ifndef World_h__
 #define World_h__
 
+#include <list>
+#include <memory>
+
 namespace MA
 {
-
+	class Entity;
     class World
     {
-
+	public:
+		static World instance;
+		std::list<std::shared_ptr<Entity>> *everybodyList()
+		{
+			return &mEverybodyList;	
+		};
+	private:
+		std::list<std::shared_ptr<Entity>> mEverybodyList;
     };
 }
 

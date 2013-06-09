7f0fbd1b62e858be3e6ca3e06f8a5b36645fc3bb
U-gandjhalf\adn <adn@gandjhalf.(none)>
2013-06-09 10:29:02 +0200

    Removed the 'old' files.

--- BUILD.sh
+++
@@ -1,1 +0,0 @@
-g++ -o game -g `find . -name '*.cpp'` -L /usr/local/lib/ -I /usr/local/include/ClanLib-2.3 -std=c++0x 2>&1 -lclan23App -lclan23Core -lclan23GL -lclan23Display -lpthread | prettybuild
